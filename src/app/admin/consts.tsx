fe479ab2fcf4c983836f9392a6994afc0b65332d
chrod <chrod.dev@gmail.com>
2024-10-16 15:51:34 -0500

    Add AdminLayout

---
+++ src/app/admin/consts.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { GrDashboard } from 'react-icons/gr';
+import { BsBoxes } from 'react-icons/bs';
+import { PiUsersThree, PiInvoice } from 'react-icons/pi';
+import { TbFileDollar, TbLogout2 } from 'react-icons/tb';
+import { IoStorefrontOutline } from 'react-icons/io5';
+import { GiCardboardBox } from 'react-icons/gi';
+import { RiExchangeBoxLine } from 'react-icons/ri';
+
+export const menuItems = [
+	{
+		key: '1',
+		icon: <GrDashboard size={20} />,
+		label: <Link href='/admin/dashboard'>Dashboard</Link>
+	},
+	{
+		key: '2',
+		icon: <BsBoxes size={20} />,
+		label: <Link href='/admin/products'>Productos</Link>
+	},
+	{
+		key: '3',
+		icon: <PiUsersThree size={20} />,
+		label: <Link href='/admin/users'>Users</Link>
+	},
+	{
+		key: '4',
+		icon: <IoStorefrontOutline size={20} />,
+		label: <Link href='/admin/stores'>Tiendas</Link>
+	},
+	{
+		key: '5',
+		icon: <TbFileDollar size={20} />,
+		label: <Link href='/admin/quotes'>Cotizaciones</Link>
+	},
+	{
+		key: '6',
+		icon: <PiInvoice size={20} />,
+		label: <Link href='/admin/invoices'>Facturas</Link>
+	},
+	{
+		key: '7',
+		icon: <GiCardboardBox size={20} />,
+		label: <Link href='/admin/stock'>Stock</Link>
+	},
+	{
+		key: '8',
+		icon: <RiExchangeBoxLine size={20} />,
+		label: <Link href='/admin/stock-transfers'>Despachos</Link>
+	},
+	{
+		key: '9',
+		icon: <TbLogout2 size={20} />,
+		label: 'Cerrar sesión',
+		onClick: () => console.log('Saliendo!!!')
+	}
+];
---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,100 @@
+'use client';
+import { useState } from 'react';
+import { Button, Layout, Menu, theme } from 'antd';
+import {
+	TbLayoutSidebarLeftCollapseFilled,
+	TbLayoutSidebarRightCollapseFilled
+} from 'react-icons/tb';
+import { PiUserCircle } from 'react-icons/pi';
+import { menuItems } from './consts';
+
+const { Header, Sider, Content } = Layout;
+const AdminLayout = ({
+	children
+}: Readonly<{
+	children: React.ReactNode;
+}>) => {
+	const [collapsed, setCollapsed] = useState(false);
+	const [selectedItem, setSelectedItem] = useState(['1']);
+	const [hide, setHide] = useState(false);
+	const {
+		token: { colorBgContainer, borderRadiusLG }
+	} = theme.useToken();
+
+	return (
+		<Layout className='h-screen'>
+			<Sider
+				className='p-2'
+				trigger={null}
+				collapsible
+				collapsed={collapsed}
+				breakpoint='md'
+				onBreakpoint={broken => {
+					setCollapsed(broken);
+					setHide(broken);
+				}}
+			>
+				<h1 className='text-center p-4 text-2xl text-white font-bold'>
+					{collapsed ? 'M' : 'MANUARTE'}
+				</h1>
+				<Menu
+					theme='dark'
+					mode='inline'
+					defaultSelectedKeys={['1']}
+					selectedKeys={selectedItem}
+					onSelect={({ key }) => (key !== '9' ? setSelectedItem([key]) : null)}
+					items={menuItems}
+				/>
+			</Sider>
+			<Layout>
+				<Header
+					style={{ padding: 0, background: colorBgContainer }}
+					className='flex justify-end md:justify-between'
+				>
+					{!hide ? (
+						<Button
+							type='text'
+							icon={
+								collapsed ? (
+									<TbLayoutSidebarRightCollapseFilled size={20} />
+								) : (
+									<TbLayoutSidebarLeftCollapseFilled size={20} />
+								)
+							}
+							onClick={() => setCollapsed(!collapsed)}
+							style={{
+								fontSize: '16px',
+								width: 64,
+								height: 64
+							}}
+						/>
+					) : null}
+					<Button
+						type='text'
+						icon={<PiUserCircle size={20} />}
+						iconPosition='end'
+						style={{
+							fontSize: '16px',
+							height: 64,
+							padding: '0 24px'
+						}}
+					>
+						Administrador
+					</Button>
+				</Header>
+				<Content
+					style={{
+						padding: 24,
+						minHeight: 280,
+						background: colorBgContainer,
+						borderRadius: borderRadiusLG
+					}}
+				>
+					{children}
+				</Content>
+			</Layout>
+		</Layout>
+	);
+};
+
+export default AdminLayout;
