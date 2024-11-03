e50a12b48101659ea3b156faacee66ebbb2a07b6
chrod <chrod.dev@gmail.com>
2024-11-03 12:12:59 -0500

    Update productCategories endpoint

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,5 @@ const BASE_URL = process.env.NEXT_APP_API_URL;
 
 export const ENDPOINTS = {
 	products: `${BASE_URL}/products`,
-	productCategories: `${BASE_URL}/products/categories`
+	productCategories: `${BASE_URL}/product-categories`
 };
