e214e456aaca72a61b7fb11549836fea233dee70
chrod <chrod.dev@gmail.com>
2024-11-07 13:22:12 -0500

    Update Modal component structure

--- src/components/layout/Modal/index.tsx
+++ src/components/layout/Modal/index.tsx
@@ -1,53 +1,26 @@
-import { Form, Modal } from 'antd';
+import { Modal } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
-import useForm from '@/hooks/useForm';
 import useModal from '@/hooks/useModal';
 import { closeModal } from '@/reducers/ui/uiSlice';
-import { ModalContentKey, ModalSubmitFnKey } from '@/enums';
+import { ModalContentKey } from '@/enums';
 
 const CustomModal = () => {
 	const { modal } = useSelector((state: RootState) => state.ui);
 	const { isOpen, title, content } = modal;
 	const dispatch = useDispatch();
-	const { form, isLoading, SUBMIT_FUNCTIONS } = useForm();
-	const { MODAL_FORM_CONTENT } = useModal();
+	const { MODAL_CONTENT } = useModal();
 
-	const modalContent = MODAL_FORM_CONTENT[content as ModalContentKey] ?? null;
-
-	const onSubmit =
-		SUBMIT_FUNCTIONS[content as ModalSubmitFnKey] ?? (() => Promise.resolve());
+	const modalContent = MODAL_CONTENT[content as ModalContentKey] ?? null;
 
 	return (
 		<Modal
 			open={isOpen}
 			title={title}
-			okText='Agregar'
-			cancelText='Cancelar'
-			okButtonProps={{
-				autoFocus: true,
-				htmlType: 'submit',
-				loading: isLoading
-			}}
-			cancelButtonProps={{
-				variant: 'outlined',
-				color: 'danger'
-			}}
 			onCancel={() => dispatch(closeModal())}
 			destroyOnClose
 			centered
 			width={500}
-			modalRender={dom => (
-				<Form
-					layout='vertical'
-					form={form}
-					name='form_in_modal'
-					initialValues={{ modifier: 'public' }}
-					clearOnDestroy
-					onFinish={values => onSubmit(values)}
-				>
-					{dom}
-				</Form>
-			)}
+			footer={null}
 		>
 			{modalContent}
 		</Modal>
--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -3,14 +3,14 @@ import ProductForm from '@/components/admin/products/ProductForm';
 import { ModalContentKey } from '@/enums';
 
 const useModal = () => {
-	const MODAL_FORM_CONTENT: Record<ModalContentKey, ReactNode> = {
+	const MODAL_CONTENT: Record<ModalContentKey, ReactNode> = {
 		[ModalContentKey.Products]: <ProductForm />,
 		[ModalContentKey.ProductVariants]: <p>Form ProductVariants</p>,
 		[ModalContentKey.ProductCategories]: <p>Form ProductCategories</p>,
 		[ModalContentKey.Users]: <p>Form Users</p>
 	};
 
-	return { MODAL_FORM_CONTENT };
+	return { MODAL_CONTENT };
 };
 
 export default useModal;
