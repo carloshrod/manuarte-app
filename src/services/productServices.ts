1cff14f08be7fd7d3e81969c0c72970e8297a46c
chrod <chrod.dev@gmail.com>
2024-11-14 16:34:29 -0500

    Update product services

--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,79 +1,57 @@
 import axios from 'axios';
 import { ENDPOINTS } from '@/config/env';
 
-export const getAllProducts = async () => {
-	try {
-		const res = await axios.get(ENDPOINTS.PRODUCTS);
+export const ProductServices = {
+	getAllProducts: async () => {
+		try {
+			const res = await axios.get(ENDPOINTS.PRODUCTS);
 
-		return res.data;
-	} catch (error) {
-		console.error(error);
-	}
-};
-
-export const getAllProductVariants = async () => {
-	try {
-		const res = await axios.get(ENDPOINTS.PRODUCT_VARIANTS);
-
-		return res.data;
-	} catch (error) {
-		console.error(error);
-	}
-};
-
-export const getAllProductCategories = async () => {
-	try {
-		const res = await axios.get(ENDPOINTS.PRODUCT_CATEGORIES);
-
-		return res.data;
-	} catch (error) {
-		console.error(error);
-	}
-};
-
-export const createProductService = async (body: SubmitProductDto) => {
-	return await axios.post(ENDPOINTS.PRODUCTS, body);
-};
-
-export const updateProductService = async (
-	body: SubmitProductDto,
-	productId: string
-) => {
-	return await axios.put(`${ENDPOINTS.PRODUCTS}/${productId}`, body);
-};
-
-export const updateProductVariantService = async (
-	body: SubmitProductDto,
-	productVariantId: string
-) => {
-	return await axios.put(`${ENDPOINTS.PRODUCT_VARIANTS}/${productVariantId}`, {
-		name: body?.productVariant?.name
-	});
-};
-
-export const addProductVariantService = async (
-	body: { name: string },
-	productId: string
-) => {
-	return axios.post(`${ENDPOINTS.PRODUCTS}/add-variant/${productId}`, body);
-};
-
-export const deleteProductService = async (
-	productId: string,
-	productVariantId: string
-) => {
-	return await axios.delete(`${ENDPOINTS.PRODUCTS}`, {
-		params: {
-			productId,
-			productVariantId
+			return res.data;
+		} catch (error) {
+			console.error(error);
 		}
-	});
-};
+	},
 
-export const searchProductsByName = async (productName: string) => {
-	const res = await axios.get(
-		`${ENDPOINTS.PRODUCTS}/search?productName=${productName}`
-	);
+	getAllProductVariants: async () => {
+		try {
+			const res = await axios.get(ENDPOINTS.PRODUCT_VARIANTS);
 
-	return res.data;
+			return res.data;
+		} catch (error) {
+			console.error(error);
+		}
+	},
+
+	createProduct: async (body: SubmitProductDto) => {
+		return await axios.post(ENDPOINTS.PRODUCTS, body);
+	},
+
+	updateProduct: async (body: SubmitProductDto, productId: string) => {
+		return await axios.put(`${ENDPOINTS.PRODUCTS}/${productId}`, body);
+	},
+
+	updateProductVariant: async (
+		body: SubmitProductDto,
+		productVariantId: string
+	) => {
+		return await axios.put(
+			`${ENDPOINTS.PRODUCT_VARIANTS}/${productVariantId}`,
+			{
+				name: body?.productVariant?.name
+			}
+		);
+	},
+
+	addProductVariant: async (body: { name: string }, productId: string) => {
+		return axios.post(`${ENDPOINTS.PRODUCTS}/add-variant/${productId}`, body);
+	},
+
+	deleteProduct: async (productId: string, productVariantId: string) => {
+		return await axios.delete(`${ENDPOINTS.PRODUCTS}`, {
+			params: {
+				productId,
+				productVariantId
+			}
+		});
+	}
 };
