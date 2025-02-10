3b168cfc4a76e0b0986ea74cb8d015de0d589d3a
chrod <chrod.dev@gmail.com>
2025-02-10 12:37:07 -0500

    Add SearchAndAddProducts component

---
+++ src/components/admin/common/input-data/SearchAndAddProducts/index.tsx
@@ -0,0 +1,112 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { Form, Select, SelectProps, Spin } from 'antd';
+import { MehOutlined } from '@ant-design/icons';
+import { getProductsData } from '@/components/admin/utils';
+
+interface SearchAndAddProductsProps {
+	onAdd: () => void;
+	selectedProduct: ProductVariantWithStock | null;
+	setSelectedProduct: Dispatch<SetStateAction<ProductVariantWithStock | null>>;
+	shopSlug?: string;
+}
+
+const SearchAndAddProducts = ({
+	onAdd,
+	selectedProduct,
+	setSelectedProduct,
+	shopSlug
+}: SearchAndAddProductsProps) => {
+	const [productsOptions, setProductsOptions] = useState<
+		SelectProps['options']
+	>([]);
+	const [isSearching, setIsSearching] = useState(false);
+	const [hasSearched, setHasSearched] = useState(false);
+	const [productsData, setProductsData] = useState<ProductVariant[]>([]);
+
+	let timeout: ReturnType<typeof setTimeout> | null;
+
+	const handleSearch = (newValue: string) => {
+		setIsSearching(true);
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = null;
+		}
+		const currentValue = newValue;
+
+		if (newValue) {
+			if (newValue.length < 3) {
+				setProductsOptions([]);
+				setIsSearching(false);
+				return;
+			}
+			setHasSearched(true);
+
+			timeout = setTimeout(async () => {
+				const res = await getProductsData({
+					currentValue,
+					newValue,
+					shopSlug
+				});
+
+				if (res) {
+					setProductsOptions(res.formattedData);
+					setProductsData(res.data);
+				}
+				setIsSearching(false);
+			}, 500);
+		} else {
+			setProductsOptions([]);
+			setIsSearching(false);
+		}
+	};
+
+	const handleSelect = (newValue: string) => {
+		const product = productsData?.find(p => p.id === newValue);
+		setSelectedProduct(product as ProductVariantWithStock);
+	};
+
+	useEffect(() => {
+		if (selectedProduct) {
+			onAdd();
+			setHasSearched(false);
+			setProductsOptions([]);
+			setProductsData([]);
+			setSelectedProduct(null);
+		}
+	}, [selectedProduct]);
+
+	const searchingOrNoContent = isSearching ? (
+		<div className='flex justify-center text-center'>
+			<Spin style={{ padding: '14px 0' }} />
+		</div>
+	) : (
+		hasSearched && (
+			<div className='text-center'>
+				<MehOutlined style={{ fontSize: 24 }} />
+				<p>Sin resultados</p>
+			</div>
+		)
+	);
+
+	return (
+		<Form.Item name='product' label='Buscar producto' style={{ maxWidth: 500 }}>
+			<Select
+				allowClear
+				showSearch
+				placeholder='Nombre o código del producto'
+				defaultActiveFirstOption={false}
+				suffixIcon={null}
+				filterOption={false}
+				onSearch={handleSearch}
+				onSelect={handleSelect}
+				notFoundContent={searchingOrNoContent}
+				options={(productsOptions || []).map(d => ({
+					value: d.value,
+					label: d.label
+				}))}
+			/>
+		</Form.Item>
+	);
+};
+
+export default SearchAndAddProducts;
